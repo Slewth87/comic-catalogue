08ea189376e74567de5cb17afda0265a7a4fed62
Richard Duffy <rduffy@slack-corp.com>
2022-06-23 21:23:50 +0100

    Added token verification to file upload. Need to revisit the status responses though. Current;y sending either status code or response body (depending on order), but not both

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,5 +1,7 @@
 var uploader = require('../processes/Uploader.js');
 var express = require('express');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 var fileUpload = require('express-fileupload');
 var cors = require('cors');
 var router = express.Router();
@@ -14,48 +16,78 @@ router.use(fileUpload({
 router.use(cors());
 router.use(morgan('dev'));
 
-router.post('/upload', async function (req, res) {
+/**
+ * @openapi
+ * /files/upload:
+ *   post:
+ *     description: 'Uploaded file in the body'
+ *     consumes:
+ *       multipart/form-data
+ *     produces:
+ *       application/json
+ *     responses:
+ *       201:
+ *         description: Returns success message confirming uploaded file
+ *       204:
+ *         description: Returns message advising no file received
+ *       401:
+ *         description: Notifies that passed token has expired
+ *       500:
+ *         description: Unkown error returned
+ */
+router.post('/upload', async function (req, res, next) {
+  console.log(req.query);
+  var token = req.query.token
   try {
-          if (!req.files) {
-            res.send({
-              status: false,
-              message: 'No file uploaded'
-            });
-          } else {
-            let comicFile = req.files.comicFile;
-            let filetype = comicFile.name.split(".").pop();
-            let supported = "cbz"
-            if (filetype == supported) {
-              let location = './uploads/' + comicFile.name;
-              let filedata = {
-                name: comicFile.name,
-                location: location
-              }
-              comicFile.mv(location);
-  
-              res.send({
-                status: true,
-                message: 'File uploaded',
-                data: filedata
-              });
-
-              uploader.upload(filedata);
-
-            } else {
-              res.send({
-                status: true,
-                message: 'Unsupported filetype'
-              });
-            }
-
+    var decoded = await jwt.verify(token, "SECRET_KEY", {complete: true});
+  } catch (error) {
+      console.log(error)
+      return res.status(401).json({
+        "message": "Session expired. Please login again"
+      })
+  }
+  if(bcrypt.compareSync(decoded.payload.user_id, decoded.payload.hash)) {
+    try {
+      if (!req.files) {
+        res.status(204).send("No file uploaded");
+      } else {
+        let comicFile = req.files.comicFile;
+        let filetype = comicFile.name.split(".").pop();
+        let supported = "cbz"
+        if (filetype == supported) {
+          let location = './uploads/' + comicFile.name;
+          let filedata = {
+            name: comicFile.name,
+            location: location
           }
-      } catch (err) {
-        console.log(err)
+          comicFile.mv(location);
 
-        res.status(500).json({
-          message: "failed upload"
-        })
+          res.status(201).json({
+            "message": "File uploaded",
+            "data": filedata
+          });
+
+          uploader.upload(filedata);
+
+        } else {
+          res.status(204).json({
+            "status": true,
+            "message": "Unsupported filetype"
+          });
+        }
       }
+    } catch (err) {
+      console.log(err)
+
+      res.status(500).json({
+        "message": "Unknown error"
+      })
+    }
+  } else {
+    return res.status(401).json({
+      "message": "Session expired. Please login again"
+    })
+  }
 });
 
 module.exports = router;
